ba01f0e16cb02812a898d9b481d70975b2561c03
Shuur <shuuriyejr@gmail.com>
2025-05-26 14:39:08 +0300

    jdbcNamedParameter

--- src/main/java/com/example/demo/UniversityService.java
+++ src/main/java/com/example/demo/UniversityService.java
@@ -1,51 +1,84 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.parsing.Location;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UniversityService {
-    private final JdbcTemplate jdbc;
+    private final JdbcTemplate jdbc;  //?
+
+    private final NamedParameterJdbcTemplate namedParameter; //Parameter
     String query = "";
 
-    public UniversityService(JdbcTemplate jdbc) {
+    public UniversityService(JdbcTemplate jdbc, NamedParameterJdbcTemplate namedParameter) {
         this.jdbc = jdbc;
+        this.namedParameter = namedParameter;
     }
 
     //operations
     //get all universities
     public List<University> getAll() {
         query = "select * from university";
-        return jdbc.query(query,
+//        return jdbc.query(query,
+//                new BeanPropertyRowMapper<>(University.class));
+        return namedParameter.query(query ,
                 new BeanPropertyRowMapper<>(University.class));
     }
 
     //get univ by id
     public University getUnivById(int id) {
-        query = "select * from university where id = ?"; // ? placeholder
-    return jdbc.queryForObject(query ,
-            new Object[]{id},
-            new BeanPropertyRowMapper<>(University.class));
+        //query = "select * from university where id = ?"; // ? placeholder
+        query = "select * from university where id = :id"; // :Parameter
+        MapSqlParameterSource source = new MapSqlParameterSource()
+                .addValue("id" , id);
+        return namedParameter.queryForObject(query ,
+                source ,
+                new BeanPropertyRowMapper<>(University.class)
+                );
+//    return jdbc.queryForObject(query ,
+//            new Object[]{id},
+//            new BeanPropertyRowMapper<>(University.class));
     }
 
     //add university
     public void addUniversity(University university){
-        query = "insert into university values(? , ? , ?)";
-        jdbc.update(query , university.getId() , university.getName() , university.getLocation());
+       // query = "insert into university values(? , ? , ?)";
+        query = "insert into university values(:id , :name , :location)";
+        MapSqlParameterSource source = new MapSqlParameterSource()
+                .addValue("name" , university.getName())
+                .addValue("location" , university.getLocation())
+                .addValue("id" , university.getId());
+        namedParameter.update(query , source);
+
+        //jdbc.update(query , university.getId() , university.getName() , university.getLocation());
     }
 
     // update
     public void updateUniversity(University newUniversity, int id){
-        query = "update university set name = ? , location=? where id=?";
-        jdbc.update(query , newUniversity.getName(),newUniversity.getLocation(),id);
-    }
+//        query = "update university set name = ? , location=? where id=?";
+//        jdbc.update(query , newUniversity.getName(),newUniversity.getLocation(),id);
+//
+        query = "update university set name = :name , location=:location where id=:id";
+        MapSqlParameterSource source= new MapSqlParameterSource()
+                .addValue("name",newUniversity.getName())
+                .addValue("id",id)
+                .addValue("location", newUniversity.getLocation());
+        namedParameter.update(query,source);
+}
 
     //delete
     public void deleteUniversity(int id){
-        query = "delete from university where id =?";
-        jdbc.update(query , id);
+        //query = "delete from university where id =?";
+        query = "delete from university where id = :id";
+        MapSqlParameterSource parameterSource = new MapSqlParameterSource()
+                .addValue("id" , id);
+        namedParameter.update(query , parameterSource);
+        //jdbc.update(query , id);
     }
 }
